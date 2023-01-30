a2697a9730f0ec359e127c3c58c8ad1cd3ac633e
gerald glatt <geraldglatt89@gmail.com>
2023-01-30 09:07:59 +0100

    Add knp_paginator

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -3,16 +3,28 @@
 namespace App\Controller\Blog;
 
 use App\Repository\Post\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController 
 {
     #[Route('/', name: 'post_index', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    public function index(
+        PostRepository $postRepository,
+        PaginatorInterface $paginator,
+        Request $request
+        ): Response
     {
-        $posts = $postRepository->findPublished();
+        $datas = $postRepository->findPublished();
+
+        $posts = $paginator->paginate(
+            $datas,
+            $request->query->getInt('page,', 1),
+            9
+        );
 
         return $this->render('pages/blog/index.html.twig', [
             'posts' =>$posts
