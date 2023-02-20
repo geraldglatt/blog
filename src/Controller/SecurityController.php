739bf8b93ff4a66b440204bd723ee320693bda70
gerald glatt <geraldglatt89@gmail.com>
2023-02-20 18:26:44 +0100

    Add login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,12 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
     #[Route('/connexion', name: 'security_login', methods: ['GET', 'POST'])]
-    public function login(): Response
+    public function login(AuthenticationUtils $utils): Response
     {
-        return $this->render('pages/security/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $lastUsername = $utils->getLastUsername();
+
+        return $this->render('pages/security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
     }
 }
